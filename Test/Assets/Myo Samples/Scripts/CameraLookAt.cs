5731d399321dec53dfd8faa0defba77caa1caeeb
Patrick Bardo <dr.p.bardo@gmail.com>
2017-03-25 10:52:42 -0400

    Initial commit of work done

---
+++ Test/Assets/Myo Samples/Scripts/CameraLookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPostition = new Vector3( GameObject.Find ("Stick").transform.position.x, 
+			this.transform.position.y, 
+			GameObject.Find ("Stick").transform.position.z ) ;
+		this.transform.LookAt( targetPostition ) ;
+	}
+}
